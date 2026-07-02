4a5b6e4743afbc9fe16a5e91151b2d4ab664d51b
joelcw <joel.wallenberg@gmail.com>
2026-07-02 11:56:18 +0100

    created shell scrip for fixing stuff from parsed file for annotation

---
+++ scripts/runOnRawParsed.sh
@@ -0,0 +1,19 @@
+CS="java -classpath CS_2.002.75.jar csearch/CorpusSearch"
+
+cp $1 temp.psd
+FILE="temp.psd"
+rm problemfile.txt
+
+
+echo ""
+echo "Fix lekh"
+$CS structure_queries/fix-lekh.q $FILE >> problemfile.txt
+mv -f $FILE.out $FILE
+
+python3 "pycConversions.py" $FILE > $FILE.out
+mv -f $FILE.out $FILE
+
+mv -f $FILE $2
+
+grep -a12 "WARNING" problemfile.txt
+
