a4f73d8324d27763c93caa374e8fb2c006a1093c
mkaps <marju.kaps@ncl.ac.uk>
2026-09-22 12:43:51 +0100

    joel fixed script

--- scripts/concatenateAndAddCarriageReturn.py
+++ scripts/concatenateAndAddCarriageReturn.py
@@ -3,7 +3,14 @@ from glob import glob
 
 filelist = glob('%s' % sys.argv[1])
 
-for file in filelist:
+filelist.sort()
+
+ii = 0
+
+while (ii < len(filelist)):
+    file = filelist[ii]
+    sys.stderr.write(file)
     string = open(file,"r").read()
     sys.stdout.write("%s\n\n" % string)
+    ii += 1
 
