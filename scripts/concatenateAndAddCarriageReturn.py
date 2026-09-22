6eec8d4fe9a7b1327c87f8167985a98c761fecf3
joelcw <joel.wallenberg@gmail.com>
2026-09-22 11:23:29 +0100

    fixed concatenating script

--- scripts/concatenateAndAddCarriageReturn.py
+++ scripts/concatenateAndAddCarriageReturn.py
@@ -1,11 +1,9 @@
-import sys,string
+import sys,string, fileinput
 from glob import glob
 
 filelist = glob('%s' % sys.argv[1])
 
-
 for file in filelist:
-
     string = open(file,"r").read()
     sys.stdout.write("%s\n" % string)
 
